7c922e9002e11f1cac949a811c9aedc7ef56fd75
Kathy Vira <katy2002@gmail.com>
2020-11-04 17:44:09 +0200

    added add item

--- shop/models.py
+++ shop/models.py
@@ -47,12 +47,10 @@ class Product(models.Model):
 
 
 class Cart(models.Model):
-	"""docstring for Cart"""
-		
 	cart_id = models.CharField(max_length=250, blank=True)
-	date_added =  models.DateTimeField(auto_now_add=True)
+	date_added = models.DateField(auto_now_add=True)
 	class Meta:
-		ordering =['date_added']
+		ordering = ['date_added']
 		db_table = 'Cart'
 
 
@@ -61,21 +59,20 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
-	"""docstring for CartItem"""
-
 	product = models.ForeignKey(Product, on_delete=models.CASCADE)
-	cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+	cart = models.ForeignKey(Cart, on_delete= models.CASCADE)
 	quantity = models.IntegerField()
 	active = models.BooleanField(default=True)
 
 	class Meta:
 		db_table = 'CartItem'
 
+
 	def sub_total(self):
 		return self.product.price * self.quantity
 
+
 	def __str__(self):
 		return self.product
-
 
 
--- shop/urls.py
+++ shop/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('category/<slug:category_slug>', views.home, name='products_by_category'),
     path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
-    path('cart', views.cart, name='cart'),
+    path('cart', views.cart_detail, name='cart_detail'),
+    path('cart/add/<int:product_id>', views.add_cart, name='add_cart'),
+    path('cart/remove/<int:product_id>', views.cart_remove, name='cart_remove'),
 ]
